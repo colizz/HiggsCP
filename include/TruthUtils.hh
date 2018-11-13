82bba510d8c4f2505f0ab72a1f71eaa9540a8012
Congqiao Li <congqiao.li@cern.ch>
2018-11-13 11:58:23 +0800

    create TrurhUtils

---
+++ include/TruthUtils.hh
@@ -0,0 +1,22 @@
+#ifndef _TruthUtils_hh_
+#define _TruthUtils_hh_
+
+#include <Utils.hh>
+#include <marlin/Processor.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <EVENT/LCCollection.h>
+#include <IMPL/LCCollectionVec.h>
+#include <EVENT/MCParticle.h>
+
+using namespace std;
+using namespace lcio;
+using namespace marlin;
+
+int IsTruthHtt (LCCollection* col_MCP) ;
+std::pair<enum TauMode, enum TauMode> GetTruthTauDecayMode (LCCollection* col_MCP);
+
+#endif
+
+
